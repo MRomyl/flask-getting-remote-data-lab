02afe958b53c51ef64a34ab7269800af35e3ea3c
MRomyl <romyl.martushev@gmail.com>
2025-11-12 21:53:30 -0700

    done

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,10 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.text
 
     def load_json(self):
-        pass
+        reponse_body = self.get_response_body()
+        data = json.loads(reponse_body)
+        return data
